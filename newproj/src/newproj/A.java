55a6e679e64f1bf6b4abb6f69f8b6f6dfff87dea
madhan <madhan15996@gmail.com>
2018-12-04 00:51:45 +0530

    firdt commit

---
+++ newproj/src/newproj/A.java
@@ -0,0 +1,14 @@
+package newproj;
+
+public class A 
+{
+public static void main (String[] args)
+{
+	System.out.println("haiiii maddyy");
+	int a=5;
+	int b=6;
+	int c=a+b;
+	System.out.println("the result is  "+c);	
+
+}
+}
